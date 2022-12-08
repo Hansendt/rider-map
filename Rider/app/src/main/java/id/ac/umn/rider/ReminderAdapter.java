ff0439af0d4138e1b65aa9424a1fd90803ba9019
aldooswald <putra.oswald@student.umn.ac.id>
2022-12-08 11:22:14 +0700

    reminder checklist

--- Rider/app/src/main/java/id/ac/umn/rider/ReminderAdapter.java
+++ Rider/app/src/main/java/id/ac/umn/rider/ReminderAdapter.java
@@ -1,14 +1,18 @@
 package id.ac.umn.rider;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.media.Image;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -44,15 +48,50 @@ public class ReminderAdapter extends RecyclerView.Adapter<ReminderAdapter.MyView
         return reminderList.size();
     }
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
+    public class MyViewHolder extends RecyclerView.ViewHolder {
         ImageView reminderImage;
         TextView reminderPart, reminderDate, reminderOptimalDate;
+        ImageView reminderDelete;
+
         public MyViewHolder(@NonNull ViewGroup itemView) {
             super(itemView);
             reminderImage = itemView.findViewById(R.id.reminderImage);
             reminderPart = itemView.findViewById(R.id.reminderPart);
             reminderDate = itemView.findViewById(R.id.reminderDate);
             reminderOptimalDate = itemView.findViewById(R.id.reminderOptimalDate);
+//            ImageView reminderDelete= (ImageView) findViewById(R.id.image);
+//            reminderDelete.setImageResource(R.drawable.my_image);
+
+
+            reminderDelete.setOnClickListener(new View.OnClickListener() {
+//                reminderDelete.setImageResource(R.drawable.ic_baseline_check_box_24);
+                @Override
+                public void onClick(View v) {
+                    AlertDialog.Builder alert;
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        alert = new AlertDialog.Builder(itemView.getContext(), android.R.style.Theme_Material_Dialog_Alert);
+                    } else {
+                        alert = new AlertDialog.Builder(itemView.getContext());
+                    }
+                    alert.setTitle("Confirmation")
+                            .setMessage("Have you really changed your part or paid the tax?")
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    reminderList.remove(getAdapterPosition());
+                                    notifyItemRemoved(getAdapterPosition());
+                                }
+                            })
+                            .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {}
+                            })
+                            .show();
+                }
+            });
+
         }
+
+
+
+
     }
 }
