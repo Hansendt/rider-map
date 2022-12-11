abd818c78776bed5a9f7081a31af176570978cba
Hansendt <razernol88885092@gmail.com>
2022-12-11 23:06:43 +0700

    Add Part Java + xml

---
+++ Rider/app/src/main/java/id/ac/umn/rider/PartBanBelakang.java
@@ -0,0 +1,61 @@
+package id.ac.umn.rider;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class PartBanBelakang extends AppCompatActivity {
+    TextView tanggalLama, tanggalBaru;
+    Button updateTanggal;
+    String tanggalTerakhir, tanggalOptimal;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://rider-6018c-default-rtdb.firebaseio.com/");
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_list_part_banbelakang);
+
+        tanggalLama = findViewById(R.id.editText);
+        tanggalBaru = findViewById(R.id.tanggalBaru);
+        updateTanggal = findViewById(R.id.update);
+        mAuth = FirebaseAuth.getInstance();
+
+        updateTanggal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(PartBanBelakang.this, (view1, year, month, dayOfMonth) -> {
+                    tanggalLama.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
+                    tanggalBaru.setText(dayOfMonth + "/" + (month + 1) + "/" + (year+2));
+                }, 2021, 0, 1);
+                datePickerDialog.show();
+            }
+        });
+    }
+
+    public void confirm(View view) {
+
+        tanggalTerakhir = tanggalLama.getText().toString();
+        tanggalOptimal = tanggalBaru.getText().toString();
+
+        int image = R.drawable.ban_depan;
+
+        databaseReference.child("Users").child(mAuth.getCurrentUser().getUid()).child("Reminder").child("Ban Depan").child("date").setValue(tanggalTerakhir);
+        databaseReference.child("Users").child(mAuth.getCurrentUser().getUid()).child("Reminder").child("Ban Depan").child("dateOptimal").setValue(tanggalOptimal);
+        databaseReference.child("Users").child(mAuth.getCurrentUser().getUid()).child("Reminder").child("Ban Depan").child("image").setValue(image);
+
+        Intent intent = new Intent(PartBanBelakang.this, ListPart.class);
+        startActivity(intent);
+        finish();
+    }
+}
